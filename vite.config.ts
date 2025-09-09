6f80df17be430d96a45edba1b9185e717d995605
Neo-xDevx <eliancarlosramos@gmail.com>
2025-09-09 12:32:22 -0500

    fix: Actualizada configuración para Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/proyecto-Calendario-F1-2025/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
